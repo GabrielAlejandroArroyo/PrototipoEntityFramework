e42fc0978230a216d058669c82e833215fb252ee
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-22 23:21:59 -0300

    Creando Migraciones y actualizando ocreando en Base de datos

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -15,5 +15,7 @@ namespace PrototipoEntityFramework
         // Creo una table a partir de una clase
         // Las Clase Genero, se mapeara en la base de datos como nombre de tabla Generos
         public DbSet<Genero> Generos => Set<Genero>();
+
+        
     }
 }
