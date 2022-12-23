dc843d00fa125ca1f4c980b0d171348a8bb86d7b
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-22 23:04:14 -0300

    Notas sobre asignacion de clases a tablas en DbContext

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace PrototipoEntityFramework
 
         }
 
+        // Impórtante para mapear en el orm las clase con la table de base de datos uso  public DbSet<Nombre de clase que representa una entidad > Nombre de la Tabla => Set<nombre de clase entidad>();
         // Creo una table a partir de una clase
         // Las Clase Genero, se mapeara en la base de datos como nombre de tabla Generos
         public DbSet<Genero> Generos => Set<Genero>();
