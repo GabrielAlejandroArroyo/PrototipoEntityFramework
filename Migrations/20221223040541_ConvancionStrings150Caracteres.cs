a84dba2dc1aed41723f1a925a8282ff9af25b578
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-23 01:14:20 -0300

    Convencion 150 Caracteres

---
+++ Migrations/20221223040541_ConvancionStrings150Caracteres.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrototipoEntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConvancionStrings150Caracteres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
